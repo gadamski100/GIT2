ca5dc059dfb6f375973215bd806baf3a3d369a15
gadamski <gadamski@student.wszib.edu.pl>
2023-11-04 14:37:54 +0100

    Revert "Przypadkowe coś"
    This reverts commit 3f62df1938d16e37465df53a32f570a557f7cd95.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,7 @@ public class HelloWorld {
 
         for (int i = 0; i < 10; i++){
             System.out.println("Hello World!");
-
         }
-        System.out.println("widzę że bardzo lubisz się potwarzać");
+
     }
 }
